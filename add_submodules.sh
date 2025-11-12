4469c39797546f96bcaf12273999603de1b5a4d2
Admin <ngpkhanh08@gmail.com>
2025-11-12 08:44:35 +0700

    Updated .gitignore

--- add_submodules.sh
+++ add_submodules.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8 # LC_ALL overrides other LC_* variables
+
 cd build/_deps/flac-src
-git add .
-git commit -m "Update flac-src to latest"
+LANG=en_US.UTF-8 git add .
+LANG=en_US.UTF-8 git commit -m "Update flac-src to latest"
 
 cd ../freetype-src
 git add .
